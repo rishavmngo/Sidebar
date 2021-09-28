8cb0026ab9ec8b01210624cbc754e9194ac6925a
Rishav-mngo <rishavinmngo@gmail.com>
2021-09-29 00:32:11 +0530

    adding localStorage for state remembering

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const collapseBtn = document.querySelector(".collapse-btn");
 const navBar = document.querySelector(".nav-container");
-
 collapseBtn.addEventListener("click", actionCollapse);
-
+var data = [];
+console.log(navBar.classList);
+window.addEventListener("load", function () {
+  data = JSON.parse(localStorage.getItem("state"));
+  if (data.length === 0) {
+    localStorage.setItem("state", JSON.stringify(Array.from(navBar.classList)));
+  } else {
+    var temp = "";
+    data.forEach(function (item) {
+      temp = temp + item;
+      temp = temp + " ";
+    });
+    navBar.classList = temp;
+  }
+});
 function actionCollapse() {
   navBar.classList.toggle("collapse");
+  localStorage.setItem("state", JSON.stringify(Array.from(navBar.classList)));
 }
